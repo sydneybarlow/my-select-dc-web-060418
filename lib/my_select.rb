d26338cc103d476bc05968c3a7a0cd7d97e09295
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 18:06:17 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,39 @@
 def my_select(collection)
- # your code here!
+    i = 0 
+    new_collection = []
+    while i < collection.length
+      if yield(collection[i])
+        new_collection << collection[i]
+      end
+      i += 1
+    end
+  return new_collection
 end
+
+
+# def my_collect(array)
+#     i = 0 
+#     new_array = []
+#     while i < array.length
+#       new_array << yield(array[i]) 
+#       i += 1
+#     end
+#   new_array
+# end
+
+# def my_each(array)
+#   i = 0 
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+#   array
+# end
+
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end

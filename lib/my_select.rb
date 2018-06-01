12acbc3380d4fb2a97ed0461f4d7a2ea0e57c55e
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 17:43:56 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,38 @@
 def my_select(collection)
- # your code here!
+    i = 0 
+    new_collection = []
+    while i < collection.length
+    binding.pry 
+      new_collection << yield(collection[i])
+      i += 1 
+    end
+  return new_collection
 end
+
+
+# def my_collect(array)
+#     i = 0 
+#     new_array = []
+#     while i < array.length
+#       new_array << yield(array[i]) 
+#       i += 1
+#     end
+#   new_array
+# end
+
+# def my_each(array)
+#   i = 0 
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+#   array
+# end
+
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end
